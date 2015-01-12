f6875805c513ac390a301453adb30a2d4a6b095f
SKOLZ <gabriel.zanzotti@gmail.com>
2015-01-11 22:23:15 -0300

    [master] project kickoff and character assets added.

---
+++ Fatal-Fray/Assets/Scripts/MovementScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementScript : MonoBehaviour {
+
+	// Use this for initialization
+    public float minSpeed;
+    public float maxSpeed;
+    public float speed;
+    public Animator anim;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            anim.SetBool("moving", true);
+            rigidbody.velocity = new Vector3(speed, 0, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            anim.SetBool("moving", true);
+            rigidbody.velocity = new Vector3(-speed, 0, 0);
+        }
+
+        if (rigidbody.velocity.sqrMagnitude < minSpeed) {
+            rigidbody.velocity = Vector3.zero;
+            anim.SetBool("moving", false);
+        } else {
+            anim.SetBool("moving", true);
+        }
+	}
+}
---
+++ Fatal-Fray/Assets/Scripts/ShootScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootScript : MonoBehaviour {
+
+	// Use this for initialization
+    public Animator anim;
+
+	// Update is called once per frame
+	void FixedUpdate () {
+        if ((Input.GetKeyDown(KeyCode.A))) {
+            anim.SetBool("shoot", true);
+        } else {
+            anim.SetBool("shoot", false);
+
+        }
+    }
+}
