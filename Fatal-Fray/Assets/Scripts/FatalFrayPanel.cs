3990905dffa66596bdb1d6ae589173bea8860459
SKOLZ <gabriel.zanzotti@gmail.com>
2015-07-03 01:16:49 -0300

    [master] implemented all the main menu.

---
+++ Fatal-Fray/Assets/Scripts/FatalFrayPanel.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FatalFrayPanel : MonoBehaviour {
+
+	private Animator anim;
+
+	public void show() {
+		gameObject.SetActive (true);
+		anim = gameObject.GetComponent<Animator> ();
+		anim.Play("Visible");
+	}
+
+	public void hide() {
+		if (anim == null) return;
+		anim.Play("Hidden");
+		Invoke ("deactivate", 0.5f);
+	}
+
+	public void deactivate() {
+		gameObject.SetActive (false);
+	}
+
+	public void activate() {
+		gameObject.SetActive (true);
+	}
+
+}
---
+++ Fatal-Fray/Assets/Scripts/MenuSoundManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuSoundManager : MonoBehaviour {
+
+	public AudioClip buttonHighlighted;
+	public AudioClip buttonClicked;
+
+	public void playClick() {
+		audio.clip = buttonClicked;
+		audio.Play ();
+	}
+
+	public void playHighlighted() {
+		audio.clip = buttonHighlighted;
+		audio.Play ();
+	}
+	                             
+}
